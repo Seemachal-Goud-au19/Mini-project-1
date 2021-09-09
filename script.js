e0801ba3ef6acc9e04505e25b929ba19fbdee821
Seemachal Goud <80570045+Seemachal-Goud-au19@users.noreply.github.com>
2021-09-10 01:49:38 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu=document.querySelector('.menu')
+const navbar=document.querySelector('.navbar')
+const navlist=document.querySelector('.nav-link')
+menu.addEventListener('click',()=>{
+    navbar.classList.toggle('change')
+    nav-link.classList.toggle('change')
+})
+
